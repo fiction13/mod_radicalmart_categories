9a3ea8e86566cda6124812a8be0d2df676303e70
fiction13 <fiction13@yandex.ru>
2022-12-21 10:15:46 +0300

    Fix images

--- layouts/radicalmart_categories/grid.php
+++ layouts/radicalmart_categories/grid.php
@@ -10,27 +10,31 @@
 
 defined('_JEXEC') or die;
 
-use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\Component\RadicalMart\Site\Helper\MediaHelper;
 
 /* @var object $displayData Category object */
 
 ?>
 <a class="radicalmart-categories__item uk-card uk-card-small uk-card-default uk-display-block uk-link-reset"
    href="<?php echo $displayData->link; ?>">
-	<div class="uk-card-media-top uk-display-block uk-inline-clip uk-transition-toggle">
-		<div class="uk-cover-container uk-height-medium uk-width-1-1 uk-transition-scale-up uk-transition-opaque ">
-			<?php if ($icon = $displayData->media->get('image', $displayData->media->get('icon')))
-			{
-				echo HTMLHelper::image($icon, htmlspecialchars($displayData->title), array('uk-cover' => ''));
-			}
-			else
-			{
-				echo HTMLHelper::image('com_radicalmart/no-image.svg', htmlspecialchars($displayData->title),
-					array('uk-cover' => ''), true);
-			} ?>
-		</div>
-	</div>
-	<div class="uk-card-body uk-card-small">
-		<h2 class="uk-card-title uk-margin-small-bottom"><?php echo $displayData->title; ?></h2>
-	</div>
+    <div class="uk-card-media-top uk-display-block uk-inline-clip uk-transition-toggle">
+        <div class="uk-cover-container uk-height-medium uk-width-1-1 uk-transition-scale-up uk-transition-opaque ">
+			<?php echo MediaHelper::renderImage(
+				'mod_radicalmart_categories.grid',
+				$displayData->media->get('image', $displayData->media->get('icon')),
+				[
+					'alt'      => $displayData->title,
+					'loading'  => 'lazy',
+					'uk-cover' => ''
+				],
+				[
+					'category_id' => $displayData->id,
+					'no_image'    => true,
+					'thumb'       => true,
+				]); ?>
+        </div>
+    </div>
+    <div class="uk-card-body uk-card-small">
+        <h2 class="uk-card-title uk-margin-small-bottom"><?php echo $displayData->title; ?></h2>
+    </div>
 </a>
--- layouts/radicalmart_categories/overlay.php
+++ layouts/radicalmart_categories/overlay.php
@@ -12,23 +12,27 @@ defined('_JEXEC') or die;
 
 /* @var object $displayData Category object */
 
-use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\Component\RadicalMart\Site\Helper\MediaHelper;
 
 ?>
 <a class="radicalmart-categories__item uk-display-block uk-inline-clip uk-card uk-overflow-hidden uk-link-reset uk-transition-toggle"
    href="<?php echo $displayData->link; ?>">
-	<div class="uk-cover-container uk-height-medium uk-transition-scale-up uk-transition-opaque">
-		<?php if ($icon = $displayData->media->get('image', $displayData->media->get('icon')))
-		{
-			echo HTMLHelper::image($icon, htmlspecialchars($displayData->title), array('uk-cover' => ''));
-		}
-		else
-		{
-			echo HTMLHelper::image('com_radicalmart/no-image.svg', htmlspecialchars($displayData->title),
-				array('uk-cover' => ''), true);
-		} ?>
-	</div>
-	<div class="uk-overlay uk-overlay-primary uk-light uk-position-bottom">
+    <div class="uk-cover-container uk-height-medium uk-transition-scale-up uk-transition-opaque">
+		<?php echo MediaHelper::renderImage(
+			'mod_radicalmart_categories.overlay',
+			$displayData->media->get('image', $displayData->media->get('icon')),
+			[
+				'alt'      => $displayData->title,
+				'loading'  => 'lazy',
+				'uk-cover' => ''
+			],
+			[
+				'category_id' => $displayData->id,
+				'no_image'    => true,
+				'thumb'       => true,
+			]); ?>
+    </div>
+    <div class="uk-overlay uk-overlay-primary uk-light uk-position-bottom">
 		<?php echo $displayData->title; ?>
-	</div>
+    </div>
 </a>
